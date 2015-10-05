b534297d61fb6660706e700aa941f742e01457e4
Aiden Tepper <aidenjtep@gmail.com>
2015-10-05 15:12:04 -0700

    Added descriptions

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -1,11 +1,11 @@
 var xPlanes = {
-  "X-1":"description",
-  "X-3":"description",
-  "X-15":"description",
-  "X-29":"description",
-  "X-31":"description",
-  "X-37":"description",
-  "X-45":"description"
+  "X-1":"First aircraft to break the sound barrier in level flight",
+  "X-3":"Titanium alloy construction; low aspect ratio wings",
+  "X-15":"First manned hypersonic aircraft; capable of suborbital spaceflight",
+  "X-29":"Forward-swept wing testbed",
+  "X-31":"Thrust vectoring supermaneuverability testbed",
+  "X-37":"Reusable orbital spaceplane",
+  "X-45":"Unmanned combat air vehicle (UCAV) demonstrator"
 };
 
 var e = document.getElementById("planes-list");
