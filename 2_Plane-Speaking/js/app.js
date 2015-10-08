2de1571599c24e5b1cdd7f0c72d3b47feea2f150
Aiden Tepper <aidenjtep@gmail.com>
2015-10-08 11:57:34 -0700

    Fixed bugs

--- 2_Plane-Speaking/js/app.js
+++ 2_Plane-Speaking/js/app.js
@@ -4,6 +4,6 @@ for(var value in xPlanes) {
   e.innerHTML += "<div><h2>" + value + "</h2>";
   e.innerHTML += "<p>" + xPlanes[value] + "</p>";
   e.innerHTML += "<img src='img/" + value +
-                  ".jpg' style='width:50%;height=50%;'>";
+                  ".jpg' style='width:50%;height:50%;'>";
   e.innerHTML += "<br></br></div>"
 }
