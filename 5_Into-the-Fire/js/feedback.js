afbcbc138c0d0f9afb5d7b1cdf1a1499e5c40944
Aiden Tepper <aidenjtep@gmail.com>
2015-11-19 19:32:01 -0800

    Added firebase functions

--- 5_Into-the-Fire/js/feedback.js
+++ 5_Into-the-Fire/js/feedback.js
@@ -1,12 +1,48 @@
-var ref = new Firebase(""); //Use your app's Firebase URL
-
-// Add some event handlers for FB events
-
 $(document).ready(function(){
   console.log("Document loaded");
+
+  var firebase = new Firebase("https://into-the-5ire.firebaseio.com/");
+
+  firebase.on("value", function(snapshot){
+    var data = snapshot.val();
+    console.log("Snapshot recieved:");
+    console.log(data);
+    update(data);
+  }, function(errorObject){
+    console.log("The read failed: " + errorObject.code);
+  });
+
+  function update(data){
+    var feedbackList =
+    "<div>Name: " + data.fullname +
+    "<div>Email: " + data.email +
+    "<div>Feedback: " + data.feedback;
+    console.log("Data: " + JSON.stringify(data));
+    console.log("Data0: " + data[1]);
+    $("#feedback-list").html(feedbackList)
+  }
+
   $("#add-form").submit(function(event){
     event.preventDefault();
-    console.log("Form submitted!");
-    // Go grab your info and do something with it.
+    console.log("Form submitted");
+
+    var firstname = $("#firstname").val();
+    var lastname = $("#lastname").val();
+    var email = $("#email").val();
+    var feedback = $("#feedback").val();
+
+    var JSONObj = {
+      "firstname":firstname,
+      "lastname":lastname,
+      "email":email,
+      "feedback":feedback,
+      "fullname":function(){return this.firstname + " " + this.lastname;}
+    };
+
+    firebase.push(JSONObj, function(){
+      console.log("Push to Firebase was successful");
+    });
+
   });
+
 });
